8545a47724c544bacf60f142da8139ba2b27bd99
Roei Erez <roeierez@gmail.com>
2021-03-10 13:14:17 +0200

    remove unneeded color for app bar theme

--- lib/ui/themes.dart
+++ lib/ui/themes.dart
@@ -54,7 +54,6 @@ ThemeData _buildLightTheme() {
     appBarTheme: base.appBarTheme.copyWith(
       backgroundColor: Colors.white,
       foregroundColor: Colors.black,
-      color: Colors.white,
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: OutlinedButton.styleFrom(primary: Colors.grey[800]),
