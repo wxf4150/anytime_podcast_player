4ae9d532f83fc9e376b3cee1e34e8fc66417ecd8
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-07-29 16:38:12 +0300

    Set isMinimized to false by default

--- lib/ui/widgets/slider_handle.dart
+++ lib/ui/widgets/slider_handle.dart
@@ -10,7 +10,7 @@ class SliderHandle extends StatelessWidget {
 
   const SliderHandle({
     this.scrollPos,
-    this.isMinimized,
+    this.isMinimized = false,
     Key key,
   }) : super(key: key);
 
